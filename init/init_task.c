35da3e355b19f6569a1f202c1adc6f86d42ce7c9
John Stultz <johnstul@us.ibm.com>
2009-07-03 08:29:58 -0500

    posix-timers: Thread posix-cpu-timers on -rt
    posix-cpu-timer code takes non -rt safe locks in hard irq
    context. Move it to a thread.
    
    [ 3.0 fixes from Peter Zijlstra <peterz@infradead.org> ]
    
    Signed-off-by: John Stultz <johnstul@us.ibm.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- init/init_task.c
+++ init/init_task.c
@@ -57,6 +57,12 @@ static struct task_integrity init_integrity =
 					INIT_TASK_INTEGRITY(init_integrity);
 #endif
 
+#if defined(CONFIG_POSIX_TIMERS) && defined(CONFIG_PREEMPT_RT_BASE)
+# define INIT_TIMER_LIST		.posix_timer_list = NULL,
+#else
+# define INIT_TIMER_LIST
+#endif
+
 /*
  * Set up the first task table, touch at your own risk!. Base=0,
  * limit=0x1fffff (=2MB)
@@ -130,6 +136,7 @@ struct task_struct init_task
 	INIT_CPU_TIMERS(init_task)
 	.pi_lock	= __RAW_SPIN_LOCK_UNLOCKED(init_task.pi_lock),
 	.timer_slack_ns = 50000, /* 50 usec default slack */
+	INIT_TIMER_LIST
 	.thread_pid	= &init_struct_pid,
 	.thread_group	= LIST_HEAD_INIT(init_task.thread_group),
 	.thread_node	= LIST_HEAD_INIT(init_signals.thread_head),
