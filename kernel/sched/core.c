8b45b06e345b0cae2eeacc99a60cf2204824ed31
Thomas Gleixner <tglx@linutronix.de>
2011-06-15 12:36:06 +0200

    hotplug: Lightweight get online cpus
    get_online_cpus() is a heavy weight function which involves a global
    mutex. migrate_disable() wants a simpler construct which prevents only
    a CPU from going doing while a task is in a migrate disabled section.
    
    Implement a per cpu lockless mechanism, which serializes only in the
    real unplug case on a global mutex. That serialization affects only
    tasks on the cpu which should be brought down.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -8881,6 +8881,7 @@ void migrate_disable(void)
 	}
 
 	preempt_disable();
+	pin_current_cpu();
 
 	migrate_disable_update_cpus_allowed(p);
 	p->migrate_disable = 1;
@@ -8946,12 +8947,15 @@ void migrate_enable(void)
 			arg.task = p;
 			arg.dest_cpu = dest_cpu;
 
+			unpin_current_cpu();
 			preempt_enable();
 			stop_one_cpu(task_cpu(p), migration_cpu_stop, &arg);
 			tlb_migrate_finish(p->mm);
+
 			return;
 		}
 	}
+	unpin_current_cpu();
 	preempt_enable();
 }
 EXPORT_SYMBOL(migrate_enable);
