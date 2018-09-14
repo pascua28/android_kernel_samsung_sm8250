551839ad6d49914e47141bd943c17e6380319cb8
YueHaibing <yuehaibing@huawei.com>
2018-09-14 01:35:11 +0000

    blk-iolatency: remove set but not used variables 'changed' and 'blkiolat'
    Fixes gcc '-Wunused-but-set-variable' warning:
    
    block/blk-iolatency.c: In function 'scale_change':
    block/blk-iolatency.c:301:7: warning:
     variable 'changed' set but not used [-Wunused-but-set-variable]
    
    block/blk-iolatency.c: In function 'iolatency_set_limit':
    block/blk-iolatency.c:765:24: warning:
     variable 'blkiolat' set but not used [-Wunused-but-set-variable]
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/blk-iolatency.c
+++ block/blk-iolatency.c
@@ -300,7 +300,6 @@ static void scale_change(struct iolatency_grp *iolat, bool up)
 	unsigned long qd = blk_queue_depth(iolat->blkiolat->rqos.q);
 	unsigned long scale = scale_amount(qd, up);
 	unsigned long old = iolat->rq_depth.max_depth;
-	bool changed = false;
 
 	if (old > qd)
 		old = qd;
@@ -310,7 +309,6 @@ static void scale_change(struct iolatency_grp *iolat, bool up)
 			return;
 
 		if (old < qd) {
-			changed = true;
 			old += scale;
 			old = min(old, qd);
 			iolat->rq_depth.max_depth = old;
@@ -318,7 +316,6 @@ static void scale_change(struct iolatency_grp *iolat, bool up)
 		}
 	} else if (old > 1) {
 		old >>= 1;
-		changed = true;
 		iolat->rq_depth.max_depth = max(old, 1UL);
 	}
 }
@@ -757,7 +754,6 @@ static ssize_t iolatency_set_limit(struct kernfs_open_file *of, char *buf,
 {
 	struct blkcg *blkcg = css_to_blkcg(of_css(of));
 	struct blkcg_gq *blkg;
-	struct blk_iolatency *blkiolat;
 	struct blkg_conf_ctx ctx;
 	struct iolatency_grp *iolat;
 	char *p, *tok;
@@ -771,7 +767,6 @@ static ssize_t iolatency_set_limit(struct kernfs_open_file *of, char *buf,
 		return ret;
 
 	iolat = blkg_to_lat(ctx.blkg);
-	blkiolat = iolat->blkiolat;
 	p = ctx.body;
 
 	ret = -EINVAL;
