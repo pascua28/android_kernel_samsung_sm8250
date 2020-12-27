3bf711dcdb64f09d6aa58493f8fd24b187604759
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-26 19:36:29 -0800

    qcacld-3.0: Nuke as much debug bloat as possible
    The overhead from all the debugging in this monstrosity of a driver is
    measurably significant. Chop it all out from the default config and fix
    the subsequent compile errors and warnings.
    
    [Tashar02: Adapt for k4.19]
    
    Co-authored-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Change-Id: Icdec7f5859e4eb7741133deaa5d23b4f5b94d7b4
    
    Signed-off-by: nerd.zenss_ <nerd.zensdev@gmail.com>
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
@@ -10271,6 +10271,7 @@ void hdd_adapter_feature_update_work_deinit(struct hdd_adapter *adapter)
 	hdd_exit();
 }
 
+#ifdef WLAN_DEBUG
 static uint8_t *convert_level_to_string(uint32_t level)
 {
 	switch (level) {
@@ -10287,6 +10288,7 @@ static uint8_t *convert_level_to_string(uint32_t level)
 		return "INVAL";
 	}
 }
+#endif
 
 /**
  * wlan_hdd_display_tx_rx_histogram() - display tx rx histogram
@@ -10296,6 +10298,7 @@ static uint8_t *convert_level_to_string(uint32_t level)
  */
 void wlan_hdd_display_tx_rx_histogram(struct hdd_context *hdd_ctx)
 {
+#ifdef WLAN_DEBUG
 	int i;
 
 #ifdef WLAN_FEATURE_DP_BUS_BANDWIDTH
@@ -10341,6 +10344,7 @@ void wlan_hdd_display_tx_rx_histogram(struct hdd_context *hdd_ctx)
 				hdd_ctx->hdd_txrx_hist[i].is_tx_pm_qos_high ?
 				"HIGH" : "LOW");
 	}
+#endif
 }
 
 /**
