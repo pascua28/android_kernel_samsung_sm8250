84d02cdcad562dc4a6e0e5ce4176333b5b3d7b71
Sultan Alsawaf <sultan@kerneltoast.com>
2021-01-09 23:00:15 -0800

    arm64: remove redefinition of ARM64_NCAPS
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- arch/arm64/include/asm/cpucaps.h
+++ arch/arm64/include/asm/cpucaps.h
@@ -55,7 +55,6 @@
 #define ARM64_SSBS				34
 #define ARM64_WORKAROUND_1188873		35
 #define ARM64_WORKAROUND_1542418		36
-#define ARM64_NCAPS				37
 
 /* kabi: reserve 38 - 62 for future cpu capabilities */
 #define ARM64_NCAPS				62
