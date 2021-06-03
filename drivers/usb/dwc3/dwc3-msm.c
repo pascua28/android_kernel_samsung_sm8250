734d72a93d33d5db8d4925dcc520d30e48ebc3c9
freak07 <michalechner92@googlemail.com>
2021-06-03 09:54:21 +0200

    treewide: use more power efficient workingqueues
    (cherry picked from commit 513bd646f713ed72cc266394c538af294a366a16)
    (cherry picked from commit c03619708b1e323681e8a87a6b10bcdc8c7703f9)
    (cherry picked from commit 6cdeaec7a67f3c2137b2fc833f88c67ac8277453)
    (cherry picked from commit 312dccc2b5a08235beed30d4a7dc67fe249aeaa1)

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -3044,7 +3044,7 @@ static int dwc3_msm_resume(struct dwc3_msm *mdwc)
 	dwc3_pwr_event_handler(mdwc);
 
 	if (pm_qos_request_active(&mdwc->pm_qos_req_dma))
-		schedule_delayed_work(&mdwc->perf_vote_work,
+		queue_delayed_work(system_power_efficient_wq, &mdwc->perf_vote_work,
 			msecs_to_jiffies(1000 * PM_QOS_SAMPLE_SEC));
 
 #if defined(CONFIG_CCIC_ALTERNATE_MODE)
@@ -4598,7 +4598,7 @@ static void msm_dwc3_perf_vote_work(struct work_struct *w)
 		 __func__, in_perf_mode, dwc->irq_cnt);
 
 	msm_dwc3_perf_vote_update(mdwc, in_perf_mode);
-	schedule_delayed_work(&mdwc->perf_vote_work,
+	queue_delayed_work(system_power_efficient_wq, &mdwc->perf_vote_work,
 			msecs_to_jiffies(1000 * PM_QOS_SAMPLE_SEC));
 }
 
@@ -4722,7 +4722,7 @@ static int dwc3_otg_start_host(struct dwc3_msm *mdwc, int on)
 				PM_QOS_CPU_DMA_LATENCY, PM_QOS_DEFAULT_VALUE);
 		/* start in perf mode for better performance initially */
 		msm_dwc3_perf_vote_update(mdwc, true);
-		schedule_delayed_work(&mdwc->perf_vote_work,
+		queue_delayed_work(system_power_efficient_wq, &mdwc->perf_vote_work,
 				msecs_to_jiffies(1000 * PM_QOS_SAMPLE_SEC));
 #if defined(CONFIG_CCIC_ALTERNATE_MODE)
 #if defined(CONFIG_CCIC_MAX77705)
@@ -4856,7 +4856,7 @@ static int dwc3_otg_start_peripheral(struct dwc3_msm *mdwc, int on)
 				PM_QOS_CPU_DMA_LATENCY, PM_QOS_DEFAULT_VALUE);
 		/* start in perf mode for better performance initially */
 		msm_dwc3_perf_vote_update(mdwc, true);
-		schedule_delayed_work(&mdwc->perf_vote_work,
+		queue_delayed_work(system_power_efficient_wq, &mdwc->perf_vote_work,
 				msecs_to_jiffies(1000 * PM_QOS_SAMPLE_SEC));
 	} else {
 		dev_info(mdwc->dev, "%s: turn off gadget %s\n",
