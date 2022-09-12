8495cecee95149f352752630fe5f507094100e62
Samuel Pascua <pascua.samuel.14@gmail.com>
2022-09-12 21:37:31 +0800

    build: build unified 4g and 5g variants, add LTO flavour
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -9,48 +9,24 @@ KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 
 echo "**********************************"
 echo "Select variant (Snapdragon only)"
-echo "(1) 4G Variant"
-echo "(2) 5G Variant"
+echo "(1) ThinLTO build"
+echo "(2) Full LTO build"
 read -p "Selected variant: " variant
 
 make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE \
 	CLANG_DIR="/home/pascua14/llvm-12/bin/" LLVM=1 CLANG_TRIPLE=$CLANG_TRIPLE r8q_defconfig
 
 if [ $variant == "1" ]; then
-	echo "
-Compiling for 4G variant
-"
-	MODEL="G780G"
+
+scripts/configcleaner "CONFIG_THINLTO"
+
+echo "CONFIG_THINLTO=y" >> out/.config
 
 elif [ $variant == "2" ]; then
-	echo "
-Compiling for 5G variant
-"
-	MODEL="G781B"
 
-	scripts/configcleaner "
-CONFIG_SAMSUNG_NFC
-CONFIG_NFC_PN547
-CONFIG_NFC_PN547_ESE_SUPPORT
-CONFIG_NFC_FEATURE_SN100U
-CONFIG_FIVE
-"
+scripts/configcleaner "CONFIG_THINLTO"
 
-	echo "
-# CONFIG_SAMSUNG_NFC is not set
-# CONFIG_NFC_PN547 is not set
-# CONFIG_NFC_PN547_ESE_SUPPORT is not set
-# CONFIG_NFC_FEATURE_SN100U is not set
-# CONFIG_FIVE is not set
-" >> out/.config
-
-fi
-
-if [ $1 == "release" ]; then
-	echo "
-Full LTO build enabled"
-
-cat arch/arm64/configs/vendor/release_defconfig >> out/.config
+echo "CONFIG_THINLTO=n" >> out/.config
 
 fi
 
@@ -67,5 +43,5 @@ if [[ -f "$IMAGE" ]]; then
 	rm AnyKernel3/*.zip > /dev/null 2>&1
 	cp $IMAGE AnyKernel3/zImage
 	cd AnyKernel3
-	zip -r9 Kernel-$MODEL.zip .
+	zip -r9 Kernel-r8q.zip .
 fi
