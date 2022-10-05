e4a2530e57222dfddd8990f4f72eb698aa24ae92
Samuel Pascua <pascua.samuel.14@gmail.com>
2022-10-05 09:02:28 +0800

    build: use LLVM_IAS
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- regen.sh
+++ regen.sh
@@ -22,6 +22,7 @@ CONFIG_NFC_FEATURE_SN100U=y
 make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 \
 	CLANG_DIR="/home/pascua14/llvm-12/bin/" \
 	LLVM=1 \
+	LLVM_IAS=1 \
 	oldconfig
 
 cp out/.config arch/arm64/configs/r8q_defconfig
