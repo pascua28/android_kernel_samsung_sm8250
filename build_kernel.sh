3320ba5ce9d0eeb1838225c9a54b37a96441f1aa
Samuel Pascua <pascua.samuel.14@gmail.com>
2022-11-08 19:10:19 +0800

    build: switch to GCC
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,21 +3,16 @@
 export ARCH=arm64
 mkdir out
 
-BUILD_CROSS_COMPILE=/home/pascua14/gcc7/bin/aarch64-linux-gnu-
-CLANG_TRIPLE=aarch64-linux-gnu-
+BUILD_CROSS_COMPILE=aarch64-linux-gnu-
 KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 
-CLANG_VERSION=14
-LLVM_BIN=/home/pascua14/llvm-$CLANG_VERSION/bin/
+make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE \
+	r8q_defconfig
 
 make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE \
-	CLANG_DIR=$LLVM_BIN LLVM=1 LLVM_IAS=1 CLANG_TRIPLE=$CLANG_TRIPLE r8q_defconfig
+	oldconfig
 
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE LLVM=1 LLVM_IAS=1 \
-	CLANG_DIR=$LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE oldconfig
-
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE LLVM=1 LLVM_IAS=1 \
-	CLANG_DIR=$LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
+make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE
 
 IMAGE="out/arch/arm64/boot/Image.gz"
 DTB_OUT="out/arch/arm64/boot/dts/vendor/qcom"
