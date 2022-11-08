11001ba524639fc7ab6fc5bad822cc24d86f1f02
Samuel Pascua <pascua.samuel.14@gmail.com>
2022-11-08 19:10:19 +0800

    build: switch to GCC
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,68 +3,16 @@
 export ARCH=arm64
 mkdir out
 
-BUILD_CROSS_COMPILE=/home/pascua14/gcc7/bin/aarch64-linux-gnu-
-CLANG_TRIPLE=aarch64-linux-gnu-
+BUILD_CROSS_COMPILE=aarch64-linux-gnu-
 KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 
-echo "**********************************"
-echo "Select variant (Snapdragon only)"
-echo "(1) ThinLTO build"
-echo "(2) Full LTO build"
-echo "(3) Non-LTO build"
-read -p "Selected variant: " variant
+make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE \
+	r8q_defconfig
 
 make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE \
-	CLANG_DIR="/home/pascua14/llvm-12/bin/" LLVM=1 LLVM_IAS=1 CLANG_TRIPLE=$CLANG_TRIPLE r8q_defconfig
+	oldconfig
 
-scripts/configcleaner "
-CONFIG_LTO
-CONFIG_THINLTO
-CONFIG_LTO_NONE
-CONFIG_LTO_CLANG
-CONFIG_CFI_CLANG"
-
-if [ $variant == "1" ]; then
-
-scripts/configcleaner "CONFIG_THINLTO"
-
-echo "
-CONFIG_LTO=y
-CONFIG_THINLTO=y
-# CONFIG_LTO_NONE is not set
-CONFIG_LTO_CLANG=y
-# CONFIG_CFI_CLANG is not set" >> out/.config
-
-CLANG_VERSION=12
-
-elif [ $variant == "2" ]; then
-
-scripts/configcleaner "CONFIG_THINLTO"
-
-echo "
-CONFIG_LTO=y
-# CONFIG_THINLTO is not set
-# CONFIG_LTO_NONE is not set
-CONFIG_LTO_CLANG=y
-# CONFIG_CFI_CLANG is not set" >> out/.config
-
-CLANG_VERSION=12
-
-elif [ $variant == "3" ]; then
-
-echo "
-CONFIG_LTO_NONE=y
-# CONFIG_LTO_CLANG is not set" >> out/.config
-
-CLANG_VERSION=14
-
-fi
-
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE LLVM=1 LLVM_IAS=1 \
-	CLANG_DIR="/home/pascua14/llvm-$CLANG_VERSION/bin/" CLANG_TRIPLE=$CLANG_TRIPLE oldconfig
-
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE LLVM=1 LLVM_IAS=1 \
-	CLANG_DIR="/home/pascua14/llvm-$CLANG_VERSION/bin/" CLANG_TRIPLE=$CLANG_TRIPLE
+make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE
 
 IMAGE="out/arch/arm64/boot/Image.gz"
 DTB_OUT="out/arch/arm64/boot/dts/vendor/qcom"
--- regen.sh
+++ regen.sh
@@ -1,27 +1,11 @@
 #!/bin/bash
 
-export ARCH=arm64
 mkdir out
 
-KERNEL_LLVM_BIN=/home/pascua14/llvm-12/bin
-KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
-
-scripts/configcleaner "
-CONFIG_SAMSUNG_NFC
-CONFIG_NFC_PN547
-CONFIG_NFC_PN547_ESE_SUPPORT
-CONFIG_NFC_FEATURE_SN100U
-"
-
-echo "
-CONFIG_SAMSUNG_NFC=y
-CONFIG_NFC_PN547=y
-CONFIG_NFC_PN547_ESE_SUPPORT=y
-CONFIG_NFC_FEATURE_SN100U=y
-" >> out/.config
+KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc"
 
 make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 \
-	CC=$KERNEL_LLVM_BIN/clang \
+	CROSS_COMPILE=aarch64-linux-gnu- \
 	oldconfig
 
 cp out/.config arch/arm64/configs/r8q_defconfig
