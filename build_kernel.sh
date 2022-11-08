6f137c07f9623b8423e37f28aafb5e642fcba436
Samuel Pascua <pascua.samuel.14@gmail.com>
2022-11-08 11:23:47 +0800

    build: use SDClang 14
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -7,7 +7,7 @@ BUILD_CROSS_COMPILE=/home/pascua14/gcc7/bin/aarch64-linux-gnu-
 CLANG_TRIPLE=aarch64-linux-gnu-
 KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 
-CLANG_VERSION=16
+CLANG_VERSION=14
 LLVM_BIN=/home/pascua14/llvm-$CLANG_VERSION/bin/
 
 make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE \
