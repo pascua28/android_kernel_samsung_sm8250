0f068f0ccd94c93abbac933dcd1ad25f40cb3b2d
Cosmin Tanislav <demonsingur@gmail.com>
2022-12-18 04:47:12 +0200

    techpack: display: use CSC for FOD dimming
    Change-Id: I713646b79c6fafda14614ff9eca742f2724420e3

--- techpack/display/msm/sde/sde_crtc.h
+++ techpack/display/msm/sde/sde_crtc.h
@@ -440,6 +440,7 @@ struct sde_crtc_state {
 	struct sde_core_perf_params new_perf;
 	int secure_session;
 	bool color_invert_on;
+	u8 fod_dim_alpha;
 };
 
 enum sde_crtc_irq_state {
@@ -904,5 +905,7 @@ static inline void sde_crtc_set_compression_ratio(
 end:
 	SDE_DEBUG("sde_crtc comp ratio: %d\n", sde_crtc->comp_ratio);
 }
+
+bool sde_crtc_is_fod_enabled(struct drm_crtc_state *state);
 
 #endif /* _SDE_CRTC_H_ */
