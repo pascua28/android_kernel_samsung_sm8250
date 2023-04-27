130354649380b0e42d4d95ab9e61ef63f8e8adf8
Samuel Pascua <pascua.samuel.14@gmail.com>
2023-04-27 17:59:42 +0800

    Revert "build: update toolchain location"
    This reverts commit 2f7014f644b549fec6fb818d70db79897a0898a6.

--- regen.sh
+++ regen.sh
@@ -5,12 +5,12 @@ mkdir out
 KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc"
 
 make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 \
-	CROSS_COMPILE=/home/pascua14/gcc-arm64/bin/aarch64-elf- \
+	CROSS_COMPILE=aarch64-linux-gnu- \
 	CROSS_COMPILE_COMPAT=/home/pascua14/gcc-arm/bin/arm-eabi- \
 	oldconfig
 
 make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 \
-	CROSS_COMPILE=/home/pascua14/gcc-arm64/bin/aarch64-elf- \
+	CROSS_COMPILE=aarch64-linux-gnu- \
 	CROSS_COMPILE_COMPAT=/home/pascua14/gcc-arm/bin/arm-eabi- \
 	savedefconfig
 
