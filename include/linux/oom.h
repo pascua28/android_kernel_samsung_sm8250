c6601e196833a8be8e7f4c5ab493bbcb3e5aae3a
Samuel Pascua <pascua.samuel.14@gmail.com>
2023-05-29 09:04:49 +0800

    Revert "mm/oom-kill: Add option to panic when killing foreground app"
    This reverts commit 0ca93a33192e50f06010e49606686b486fed5d6f.

--- include/linux/oom.h
+++ include/linux/oom.h
@@ -148,6 +148,5 @@ extern atomic64_t last_oom_jiffies;
 
 /* calls for LMK reaper */
 extern void add_to_oom_reaper(struct task_struct *p);
-extern void check_panic_on_foreground_kill(struct task_struct *p);
 #define ULMK_MAGIC "lmkd"
 #endif /* _INCLUDE_LINUX_OOM_H */
