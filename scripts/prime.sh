9e6555ba6e12274b68482f18a578db5e81a2d185
Samuel Pascua <pascua.samuel.14@gmail.com>
2023-05-29 17:57:51 +0800

    scripts: enable uffd gc and disable Samsung lmk
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- scripts/prime.sh
+++ scripts/prime.sh
@@ -5,5 +5,8 @@ chmod 755 /dev/magiskpolicy
 
 /dev/magiskpolicy --live "allow kernel exported_config_prop property_service *"
 
+setprop ro.dalvik.vm.enable_uffd_gc true
+setprop ro.slmk.enable_userspace_lmk false
+
 # Re-enable SELinux
 echo "97" > /sys/fs/selinux/enforce
