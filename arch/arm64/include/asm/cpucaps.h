7710592a5ee0c41d60518c1677f5e92c3fc3622c
Samuel Pascua <pascua.samuel.14@gmail.com>
2023-06-08 14:03:20 +0800

    arm64: cpucaps: add spectre bhb
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- arch/arm64/include/asm/cpucaps.h
+++ arch/arm64/include/asm/cpucaps.h
@@ -55,6 +55,7 @@
 #define ARM64_SSBS				34
 #define ARM64_WORKAROUND_1188873		35
 #define ARM64_WORKAROUND_1542418		36
+#define ARM64_SPECTRE_BHB			37
 
 /* kabi: reserve 38 - 62 for future cpu capabilities */
 #define ARM64_NCAPS				62
