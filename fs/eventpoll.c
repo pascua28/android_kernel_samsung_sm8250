78205014e17ea30de24be0ef3483384c527739bf
Samuel Pascua <pascua.samuel.14@gmail.com>
2023-08-01 10:55:04 +0800

    fs: eventpoll: fix build
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- fs/eventpoll.c
+++ fs/eventpoll.c
@@ -1397,7 +1397,7 @@ static int ep_create_wakeup_source(struct epitem *epi)
 	name = epi->ffd.file->f_path.dentry->d_name.name;
 
 	snprintf(buf, sizeof(buf), "epoll_%.*s_file:%s",
-		 (int)sizeof(task_comm_buf), task_comm_buf, n.name);
+		 (int)sizeof(task_comm_buf), task_comm_buf, name);
 	ws = wakeup_source_register(NULL, buf);
 	if (!ws)
 		return -ENOMEM;
