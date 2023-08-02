e6a5f698bfd3baf49ae9149b033442207ed184a9
Samuel Pascua <pascua.samuel.14@gmail.com>
2023-08-02 08:22:52 +0800

    Revert "scsi: ufs: change msleep to usleep_range"
    This reverts commit 39f7efa3585670ea8bc9badcb40b54211e655a4e.

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -6950,6 +6950,12 @@ static int ufshcd_hba_execute_hce(struct ufs_hba *hba)
 {
 	int retry;
 
+	/*
+	 * msleep of 1 and 5 used in this function might result in msleep(20),
+	 * but it was necessary to send the UFS FPGA to reset mode during
+	 * development and testing of this driver. msleep can be changed to
+	 * mdelay and retry count can be reduced based on the controller.
+	 */
 	if (!ufshcd_is_hba_active(hba))
 		/* change controller state to "reset state" */
 		ufshcd_hba_stop(hba, true);
@@ -6972,7 +6978,7 @@ static int ufshcd_hba_execute_hce(struct ufs_hba *hba)
 	 * instruction might be read back.
 	 * This delay can be changed based on the controller.
 	 */
-	usleep_range(1000, 1100);
+	msleep(1);
 
 	/* wait for the host controller to complete initialization */
 	retry = 10;
@@ -6984,7 +6990,7 @@ static int ufshcd_hba_execute_hce(struct ufs_hba *hba)
 				"Controller enable failed\n");
 			return -EIO;
 		}
-		usleep_range(5000, 5100);
+		msleep(5);
 	}
 
 	/* enable UIC related interrupts */
