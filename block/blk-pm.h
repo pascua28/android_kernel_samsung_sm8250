4e836ba66ba9977eac6afbbca658345b31dc0062
Samuel Pascua <pascua.samuel.14@gmail.com>
2023-08-04 08:01:44 +0800

    blk-pm: re-add Samsung's changes
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- block/blk-pm.h
+++ block/blk-pm.h
@@ -8,8 +8,11 @@
 #ifdef CONFIG_PM
 static inline void blk_pm_requeue_request(struct request *rq)
 {
-	if (rq->q->dev && !(rq->rq_flags & RQF_PM))
+	if (rq->q->dev && !(rq->rq_flags & RQF_PM)) {
 		rq->q->nr_pending--;
+		if (!rq->q->nr_pending)
+			pm_runtime_mark_last_busy(rq->q->dev);
+	}
 }
 
 static inline void blk_pm_add_request(struct request_queue *q,
