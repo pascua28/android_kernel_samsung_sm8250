9f1de2896869370d9d15c878185a7ccfb7efdb27
Samuel Pascua <pascua.samuel.14@gmail.com>
2023-08-28 12:04:48 +0800

    Revert "qcacld-3.0: Nuke as much debug bloat as possible"
    This reverts commit 44dea2d8adfc8e9d8e3eef1df2f9a0209a27987f.

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
@@ -9810,7 +9810,6 @@ void hdd_adapter_feature_update_work_deinit(struct hdd_adapter *adapter)
 	hdd_exit();
 }
 
-#ifdef WLAN_DEBUG
 static uint8_t *convert_level_to_string(uint32_t level)
 {
 	switch (level) {
@@ -9827,7 +9826,6 @@ static uint8_t *convert_level_to_string(uint32_t level)
 		return "INVAL";
 	}
 }
-#endif
 
 /**
  * wlan_hdd_display_tx_rx_histogram() - display tx rx histogram
@@ -9837,7 +9835,6 @@ static uint8_t *convert_level_to_string(uint32_t level)
  */
 void wlan_hdd_display_tx_rx_histogram(struct hdd_context *hdd_ctx)
 {
-#ifdef WLAN_DEBUG
 	int i;
 
 #ifdef WLAN_FEATURE_DP_BUS_BANDWIDTH
@@ -9883,7 +9880,6 @@ void wlan_hdd_display_tx_rx_histogram(struct hdd_context *hdd_ctx)
 				hdd_ctx->hdd_txrx_hist[i].is_tx_pm_qos_high ?
 				"HIGH" : "LOW");
 	}
-#endif
 }
 
 /**
