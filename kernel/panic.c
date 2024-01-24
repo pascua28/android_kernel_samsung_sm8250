32dc952ca24f77d04537451dab3b71674fc98ddf
Samuel Pascua <pascua.samuel.14@gmail.com>
2024-01-24 15:47:59 +0800

    kernel/panic: fix build without CONFIG_QCOM_WATCHDOG_V2
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- kernel/panic.c
+++ kernel/panic.c
@@ -149,7 +149,9 @@ void panic(const char *fmt, ...)
 
 	sec_debug_store_extc_idx(false);
 	/*To prevent watchdog reset during panic handling. */
+#ifdef CONFIG_QCOM_WATCHDOG_V2
 	emerg_pet_watchdog();
+#endif
 
 	/*
 	 * Disable local interrupts. This will prevent panic_smp_self_stop
