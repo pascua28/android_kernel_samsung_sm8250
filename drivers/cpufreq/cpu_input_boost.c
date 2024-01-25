8c3ee99f01900fa7676609e98d02f69d4d86f13c
Samuel Pascua <pascua.samuel.14@gmail.com>
2024-01-25 22:36:38 +0800

    kernel: tone down util boosting
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -20,7 +20,7 @@
 #include <uapi/linux/sched/types.h>
 #endif
 
-static unsigned int dynamic_stune_boost __read_mostly = 30;
+static unsigned int dynamic_stune_boost __read_mostly = 1;
 
 static unsigned int max_boost_freq_little __read_mostly =
 	CONFIG_MAX_BOOST_FREQ_LP;
