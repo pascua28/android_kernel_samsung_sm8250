0c058155ac43b1a8aa57d36570b0a05955510c10
Samuel Pascua <pascua.samuel.14@gmail.com>
2024-01-25 09:05:47 +0800

    Revert "mm: Replace shrinker rwsem with fine-grained locks"
    This reverts commit 4cec44ac9e083637c5bfa671cfc442a5910537df.

--- include/linux/shrinker.h
+++ include/linux/shrinker.h
@@ -2,8 +2,6 @@
 #ifndef _LINUX_SHRINKER_H
 #define _LINUX_SHRINKER_H
 
-#include <linux/rwsem.h>
-
 /*
  * This struct is used to pass information from page reclaim to the shrinkers.
  * We consolidate the values for easier extention later.
@@ -82,7 +80,6 @@ struct shrinker {
 
 	/* objs pending delete, per node */
 	atomic_long_t *nr_deferred;
-	struct rw_semaphore del_rwsem;
 };
 #define DEFAULT_SEEKS 2 /* A good number if you don't know better. */
 
