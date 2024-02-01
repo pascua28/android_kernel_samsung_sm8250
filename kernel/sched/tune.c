32d65c49aaf8ac5a332294da3a7f7fa55804b589
Samuel Pascua <pascua.samuel.14@gmail.com>
2024-02-01 10:46:42 +0800

    sched: tune: disable prefer_idle for top-app by default
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -832,7 +832,7 @@ static void write_default_values(struct cgroup_subsys_state *css)
 		{ "background",	0, 0, 0, 0 },
 		{ "foreground",	0, 0, 0, 0 },
 		{ "rt",		0, 0, 0, 0 },
-		{ "top-app",	10, 1, 0, 0 },
+		{ "top-app",	10, 0, 0, 0 },
 		{ "camera-daemon", 5, 1, 0, 0 },
 	};
 	int i;
