9eb591aac23eac03de6e989c8aa01496a27e72ad
Samuel Pascua <pascua.samuel.14@gmail.com>
2024-03-15 23:38:17 +0800

    Revert "sched/eevdf: Sort the rbtree by virtual deadline"
    This reverts commit ac3a700c00b88ff622e296545fafc2ae003ca383.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -527,7 +527,7 @@ struct sched_entity {
 	unsigned long			runnable_weight;
 	struct rb_node			run_node;
 	u64				deadline;
-	u64				min_vruntime;
+	u64				min_deadline;
 
 	struct list_head		group_node;
 	unsigned int			on_rq;
