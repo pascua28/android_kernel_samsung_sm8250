6112410623feb4fd5bd8cf3a8f47e4c3fb0aba19
Samuel Pascua <pascua.samuel.14@gmail.com>
2024-04-05 10:59:50 +0800

    schedutil: add cpumask bits
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -729,6 +729,13 @@ static int sugov_init(struct cpufreq_policy *policy)
 	struct sugov_tunables *tunables;
 	int ret = 0;
 
+	static const unsigned long lp_cpu_bits = 15;
+	const struct cpumask *const cpu_lp_mask = to_cpumask(&lp_cpu_bits);
+	static const unsigned long perf_cpu_bits = 112;
+	const struct cpumask *const cpu_perf_mask = to_cpumask(&perf_cpu_bits);
+	static const unsigned long prime_cpu_bits = 128;
+	const struct cpumask *const cpu_prime_mask = to_cpumask(&prime_cpu_bits);
+
 	/* State should be equivalent to EXIT */
 	if (policy->governor_data)
 		return -EBUSY;
