a89df60b9bc004934aec9e4258a937af763b0222
EmanuelCN <emanuelghub@gmail.com>
2024-06-24 15:00:39 +0300

    sched: Implement tapered dvfs_headroom
    The linear one is very aggressive at the top frequencies. 25% headroom
    means that we saturate the cluster very quickly. Given how expensive the
    new clusters are at the top frequencies, it'd be smarter to taper the
    headroom as we approach the max capacity of the cluster.
    
    Implement that by simply taking the headroom of the different between
    curret util and capacity of the CPU. The lower frequencies will get
    a big headroom, but as we approach max capacity, we'll get almost no
    headroom.
    
    Only CFS + RT utilization use the tapered version. irq is usually small
    and this can lead to potentially undesired boost almost when there are
    no irqs running. DL shouldn't need any headroom in general. So keeping
    it using the default non tapered version as well.
    
    Signed-off-by: Qais Yousef <qyousef@google.com>
    
    [EmanuelCN: Ported from: LineageOS/android_kernel_google_gs201@752c5f9]

--- include/linux/sched/cpufreq.h
+++ include/linux/sched/cpufreq.h
@@ -35,7 +35,7 @@ static inline unsigned long map_util_freq(unsigned long util,
 	return freq * util / cap;
 }
 
-static inline unsigned long map_util_perf(unsigned long util)
+static inline __maybe_unused unsigned long map_util_perf(unsigned long util)
 {
 	return util + (util >> 2);
 }
