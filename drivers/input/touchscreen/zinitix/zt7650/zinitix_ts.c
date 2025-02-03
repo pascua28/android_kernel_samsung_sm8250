6844f8186bcb80a545541d828ad15ecb296fb155
Samuel Pascua <pascua.samuel.14@gmail.com>
2025-02-03 13:24:14 +0800

    input: touchscreen: zt7650: fix proximity sensor on OneUI
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- drivers/input/touchscreen/zinitix/zt7650/zinitix_ts.c
+++ drivers/input/touchscreen/zinitix/zt7650/zinitix_ts.c
@@ -3598,7 +3598,9 @@ static irqreturn_t zt_touch_work(int irq, void *data)
 			if (read_data(info->client, ZT_PROXIMITY_DETECT, (u8 *)&prox_data, 2) < 0)
 				input_err(true, &client->dev, "%s: fail to read proximity detect reg\n", __func__);
 
-			prox_data = prox_data == 5 || !prox_data;
+			if (is_aosp)
+				prox_data = prox_data == 5 || !prox_data;
+
 			info->hover_event = prox_data;
 
 			input_info(true, &client->dev, "PROXIMITY DETECT. LVL = %d \n", prox_data);
