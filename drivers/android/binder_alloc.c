afb4ab991ad7bc2b86b09b2ac9c425d404890e4e
Samuel Pascua <pascua.samuel.14@gmail.com>
2025-10-23 22:55:14 +0800

    Revert "binder: fix async space check for 0-sized buffers"
    This reverts commit 05088b886fea59cc827e5b5cedb66165cf532f72.

--- drivers/android/binder_alloc.c
+++ drivers/android/binder_alloc.c
@@ -434,9 +434,6 @@ static struct binder_buffer *binder_alloc_new_buf_locked(
 		return ERR_PTR(-EINVAL);
 	}
 
-	/* Pad 0-size buffers so they get assigned unique addresses */
-	size = max(size, sizeof(void *));
-
 #ifdef CONFIG_SAMSUNG_FREECESS
 	if (is_async && (alloc->free_async_space < 3 * size
 		|| alloc->free_async_space < alloc->buffer_size/4)) {
@@ -457,6 +454,9 @@ static struct binder_buffer *binder_alloc_new_buf_locked(
 			      alloc->pid, size);
 		return ERR_PTR(-ENOSPC);
 	}
+
+	/* Pad 0-size buffers so they get assigned unique addresses */
+	size = max(size, sizeof(void *));
 
 	while (n) {
 		buffer = rb_entry(n, struct binder_buffer, rb_node);
